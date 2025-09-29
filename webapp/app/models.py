7be0f274aa4fba6e47ff955c5656d247b20142fd
Pradum <pradumchaurasiya99856@gmail.com>
2025-09-29 16:12:31 +0530

    commit

--- webapp/app/models.py
+++ webapp/app/models.py
@@ -4,7 +4,7 @@ class Customer(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     country = models.CharField(max_length=30)
-    state = models.CharField(max_length=50),
+    state = models.CharField(max_length=50)
     email = models.EmailField()
     contact = models.CharField(max_length=12)
     institution = models.TextField()
